cf34e34bf8beb0297a1475503c875f0ffaed7187
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2021-04-01 20:15:38 +0300

    7.8

--- Meal_App/lib/category_meals_screen.dart
+++ Meal_App/lib/category_meals_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+import 'categories_screen.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
+
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categoryId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("The Recipe"),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text("The Recipes for the Category"),
