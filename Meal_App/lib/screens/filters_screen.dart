077f75310ee4be3caab9119154624f0c88c1174a
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2021-04-10 11:08:33 +0300

    7.28

--- Meal_App/lib/screens/filters_screen.dart
+++ Meal_App/lib/screens/filters_screen.dart
@@ -1,8 +1,33 @@
 import 'package:Meal_App/widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routName = '/filters-screen';
+
+  @override
+  _FiltersScreenState createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _glutenFree = false;
+  var _vegetarian = false;
+  var _vegan = false;
+  var _lactoseFree = false;
+
+  Widget _buildSwitch(
+    String title,
+    String subTitle,
+    bool onOff,
+    Function update,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(subTitle),
+      value: onOff,
+      onChanged: update,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,8 +35,62 @@ class FiltersScreen extends StatelessWidget {
         title: Text('Your Filters'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('MAde it'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitch(
+                  'Gluten-Free',
+                  'Only view gluten-free meals.',
+                  _glutenFree,
+                  (newValue) {
+                    setState(() {
+                      _glutenFree = newValue;
+                    });
+                  },
+                ),
+                _buildSwitch(
+                  'Vegetarian',
+                  'Only view Vegetarian meals.',
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitch(
+                  'Vegan',
+                  'Only view Vegan meals.',
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                _buildSwitch(
+                  'Lactos-Free',
+                  'Only view lactos-free meals.',
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
